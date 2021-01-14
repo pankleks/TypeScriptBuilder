e8a1750b23991bcac62885be5d4083a10b7e38cb
Markus Johansson <markus@obviuse.se>
2021-01-14 10:25:04 +0100

    Fixed assembly compile issues

--- src/TypeScriptBuilder/Properties/AssemblyInfo.cs
+++ src/TypeScriptBuilder/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TypeScriptBuilder")]
-[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("TypeScriptBuilder")]
+//[assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bb258769-d1d0-494e-92d3-943d4da4edb1")]
+//[assembly: Guid("bb258769-d1d0-494e-92d3-943d4da4edb1")]
